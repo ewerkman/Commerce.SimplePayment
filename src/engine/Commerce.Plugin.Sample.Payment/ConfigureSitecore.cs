38d838c658a1a163481e707b6ee2c4d5a6546ed3
Erwin Werkman <werkman.erwin@gmail.com>
2019-08-08 20:25:06 +0200

    Added block to settle a simple payment

--- src/engine/Commerce.Plugin.Sample.Payment/ConfigureSitecore.cs
+++ src/engine/Commerce.Plugin.Sample.Payment/ConfigureSitecore.cs
@@ -1,8 +1,10 @@
 namespace Commerce.Plugin.Sample.Payment
 {
     using System.Reflection;
+    using Commerce.Plugin.Sample.Payment.Pipelines.Blocks;
     using Microsoft.Extensions.DependencyInjection;
     using Sitecore.Commerce.Core;
+    using Sitecore.Commerce.Plugin.Orders;
     using Sitecore.Framework.Configuration;
     using Sitecore.Framework.Pipelines.Definitions.Extensions;
 
@@ -14,7 +16,8 @@ namespace Commerce.Plugin.Sample.Payment
             services.RegisterAllPipelineBlocks(assembly);
 
             services.Sitecore().Pipelines(config => config
-                .ConfigurePipeline<IConfigureServiceApiPipeline>(configure => configure.Add<ConfigureServiceApiBlock>())
+                .ConfigurePipeline<IConfigureServiceApiPipeline>(c => c.Add<ConfigureServiceApiBlock>())
+                .ConfigurePipeline<IPendingOrdersMinionPipeline>(c => c.Add<SettleSimplePaymentBlock>().Before<ProcessProblemOrderBlock>())
                 );
 
             services.RegisterAllCommands(assembly);
---
+++ src/engine/Commerce.Plugin.Sample.Payment/Pipelines/Blocks/SettleSimplePaymentBlock.cs
@@ -0,0 +1,88 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SettleSimplePaymentBlock.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2019
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Commerce.Plugin.Sample.Payment.Pipelines.Blocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Commerce.Plugin.Sample.Payment.Components;
+    using Microsoft.Extensions.Logging;
+
+    using Sitecore.Commerce.Core;
+    using Sitecore.Commerce.Plugin.ManagedLists;
+    using Sitecore.Commerce.Plugin.Orders;
+    using Sitecore.Framework.Conditions;
+    using Sitecore.Framework.Pipelines;
+
+    [PipelineDisplayName(nameof(SettleSimplePaymentBlock)]
+    public class SettleSimplePaymentBlock : PipelineBlock<Order, Order, CommercePipelineExecutionContext>
+    {
+        /// <summary>
+        /// Gets or sets the commander.
+        /// </summary>
+        /// <value>
+        /// The commander.
+        /// </value>
+        protected CommerceCommander Commander { get; set; }
+
+        /// <inheritdoc />
+        /// <summary>Initializes a new instance of the <see cref="T:Sitecore.Framework.Pipelines.PipelineBlock" /> class.</summary>
+        /// <param name="commander">The commerce commander.</param>
+        public SettleSimplePaymentBlock(CommerceCommander commander)
+            : base(null)
+        {
+
+            this.Commander = commander;
+
+        }
+
+        /// <summary>
+        /// The execute.
+        /// </summary>
+        /// <param name="order">
+        /// The pipeline argument.
+        /// </param>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        /// <returns>
+        /// The <see cref="PipelineArgument"/>.
+        /// </returns>
+        public override async Task<Order> Run(Order arg, CommercePipelineExecutionContext context)
+        {
+            Condition.Requires(arg).IsNotNull($"{this.Name}: The order cannot be null.");
+
+            var order = arg;
+            var knownOrderStatuses = context.GetPolicy<KnownOrderStatusPolicy>();
+            if (!order.Status.Equals(knownOrderStatuses.Released, StringComparison.OrdinalIgnoreCase)
+                || !order.HasComponent<SimplePaymentComponent>())
+            {
+                return order;
+            }
+
+            var salesActivities = context.CommerceContext.GetEntities<SalesActivity>().Where(sa => sa.HasComponent<SimplePaymentComponent>()).ToList();
+            if (!salesActivities.Any())
+            {
+                return order;
+            }
+
+            var knownSalesActivityStatuses = context.GetPolicy<KnownSalesActivityStatusesPolicy>();
+            foreach (var salesActivity in salesActivities)
+            {
+                var simplePayment = salesActivity.GetComponent<SimplePaymentComponent>();
+                salesActivity.PaymentStatus = knownSalesActivityStatuses.Settled;
+                salesActivity.GetComponent<TransientListMembershipsComponent>().Memberships.Add(context.GetPolicy<KnownOrderListsPolicy>().SettledSalesActivities);
+
+                await Commander.PersistEntity(context.CommerceContext, salesActivity).ConfigureAwait(false);
+                await Commander.Pipeline<IRemoveListEntitiesPipeline>().Run(new ListEntitiesArgument(new List<string> { salesActivity.Id }, context.GetPolicy<KnownOrderListsPolicy>().SettleSalesActivities), context).ConfigureAwait(false);
+            }
+
+            return order;
+        }
+    }
+}
